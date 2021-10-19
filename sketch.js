f16d91ad1badc3f18bf266cb4138769f94dbf51f
student437 <71543319+student437@users.noreply.github.com>
2021-10-19 16:51:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var path,boy, leftBoundary,rightBoundary;
+var pathImg,boyImg;
+var i;
+
+function preload(){
+  pathImg = loadImage("path.png");
+  boyImg = loadAnimation("Runner-1.png","Runner-2.png");
+}
+
+function setup(){
+  
+  createCanvas(400,400);
+  
+// Moving background
+
+}
+
+function draw() {
+  background(0);
+ 
+  
+  drawSprites();
+}
